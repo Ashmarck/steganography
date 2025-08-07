56d21a501d787576011b66d77bd77af4187f00ef
Ashmarck <ashmarck99@gmail.com>
2025-08-07 21:37:42 +0530

    New changes

--- ImageSteganography.py
+++ ImageSteganography.py
@@ -6,23 +6,23 @@ import streamlit as st
 
 def encode(img, msg):
     
-    lang_ind, data = asyncio.run(Conversion.encode_translate(msg))
+    # lang_ind, data = asyncio.run(Conversion.encode_translate(msg))
     
-    a = [ord(i) for i in data]
-    l = len(data)
+    a = [ord(i) for i in msg]
+    l = len(msg)
     
     for i in range(len(img[0])):
         img[0][i][2] = 178
         
-    img[0][0][2] = lang_ind
-    img[0][1][2] = l
+    # img[0][0][2] = 0
+    # img[0][1][2] = l
     
     if l > 250:
         print()
     else:
             
-        for i in range(2,l+2):
-            img[0][i][2] = a[i-2]
+        for i in range(l):
+            img[0][i][2] = a[i]
         
     success, encoded_image_buffer = cv.imencode(".png", img)
 
@@ -32,21 +32,21 @@ def encode(img, msg):
     
     return encoded_image_buffer.tobytes()
 
-def decode(img, lang):
+def decode(img):
     
     asciis = []
     # lang_ind = img[0][0][2]
-    l = img[0][1][2]
+    # l = img[0][1][2]
     
-    for i in range(2,l+2):
+    for i in range(len(img[0])):
 
         if img[0][i][2] in range(31,127):
             asciis.append(img[0][i][2])
-
+        
     chars = [chr(i) for i in asciis]
     text = ''.join(chars)
     
-    data = asyncio.run(Conversion.decode_translate(text, lang))
+    # data = asyncio.run(Conversion.decode_translate(text, lang))
     
-    return data
+    return text
      
--- SteganographyGUI.py
+++ SteganographyGUI.py
@@ -42,39 +42,50 @@ if options == "Image":
                 img_bytes = img_take.getvalue()
                 np_arr = np.frombuffer(img_bytes, np.uint8)
                 img_array = cv.imdecode(np_arr, cv.IMREAD_COLOR)
-                
-                encoded_image_array = ImageSteganography.encode(img_array, text)
-                st.success("Message encoded successfully!")
-                
-                st.download_button(
-                    label="Download Encoded Image",
-                    data=encoded_image_array,
-                    file_name="encoded_image.png",
-                    mime="image/png",
-                    use_container_width=True
-                )
+                try:
+                    encoded_image_array = ImageSteganography.encode(img_array, text)
+                except:
+                    with col2:
+                        st.warning("English only!")
+                else:
+                    with col2:
+                        st.success("Message encoded successfully!")
+                        st.download_button(
+                            label="Download Encoded Image",
+                            data=encoded_image_array,
+                            file_name="encoded_image.png",
+                            mime="image/png",
+                            use_container_width=True
+                        )
             else:
-                st.warning("Please upload an image and enter a message before encoding.")
+                with col2:
+                    st.warning("Please upload an image or enter a message before encoding.")
 
     elif choose == "Decode":
         st.header("Decode a Message from an Image")
-
-        img_to_decode = st.file_uploader("Upload your encoded image:", type=["jpg", "jpeg", "png"])
-
-        if img_to_decode is not None:
-            st.image(img_to_decode, caption="Encoded Image", use_container_width=True)
-            choice = st.selectbox("Select language to decode your message:", ['afrikaans', 'albanian', 'amharic', 'arabic', 'armenian', 'azerbaijani', 'basque', 'belarusian', 'bengali', 'bosnian', 'bulgarian', 'catalan', 'cebuano', 'chichewa', 'chinese (simplified)', 'chinese (traditional)', 'corsican', 'croatian', 'czech', 'danish', 'dutch', 'english', 'esperanto', 'estonian', 'filipino', 'finnish', 'french', 'frisian', 'galician', 'georgian', 'german', 'greek', 'gujarati', 'haitian creole', 'hausa', 'hawaiian', 'hebrew', 'hebrew', 'hindi', 'hmong', 'hungarian', 'icelandic', 'igbo', 'indonesian', 'irish', 'italian', 'japanese', 'javanese', 'kannada', 'kazakh', 'khmer', 'korean', 'kurdish (kurmanji)', 'kyrgyz', 'lao', 'latin', 'latvian', 'lithuanian', 'luxembourgish', 'macedonian', 'malagasy', 'malay', 'malayalam', 'maltese', 'maori', 'marathi', 'mongolian', 'myanmar (burmese)', 'nepali', 'norwegian', 'odia', 'pashto', 'persian', 'polish', 'portuguese', 'punjabi', 'romanian', 'russian', 'samoan', 'scots gaelic', 'serbian', 'sesotho', 'shona', 'sindhi', 'sinhala', 'slovak', 'slovenian', 'somali', 'spanish', 'sundanese', 'swahili', 'swedish', 'tajik', 'tamil', 'telugu', 'thai', 'turkish', 'ukrainian', 'urdu', 'uyghur', 'uzbek', 'vietnamese', 'welsh', 'xhosa', 'yiddish', 'yoruba', 'zulu'])
-            dec_button = st.button("Decode Image", type="primary", use_container_width=True)
-
-            if dec_button:
-                img_bytes = img_to_decode.getvalue()
-                np_arr = np.frombuffer(img_bytes, np.uint8)
-                img_array = cv.imdecode(np_arr, cv.IMREAD_COLOR)
+        col1, col2 = st.columns(2)
+        with col1:
+            img_to_decode = st.file_uploader("Upload your encoded image:", type=["jpg", "jpeg", "png"])
+            if img_to_decode is not None:
+                st.image(img_to_decode, caption="Encoded Image", use_container_width=True)
+            
+        with col2:
+            st.markdown("")
+            st.markdown("")
+            try:
+                dec_button = st.button("Decode Image", type="primary", use_container_width=True)
+                if dec_button:
+                    img_bytes = img_to_decode.getvalue()
+                    np_arr = np.frombuffer(img_bytes, np.uint8)
+                    img_array = cv.imdecode(np_arr, cv.IMREAD_COLOR)
                 
-                decoded_message = ImageSteganography.decode(img_array, choice)
+                    decoded_message = ImageSteganography.decode(img_array)
                 
-                st.success("Message decoded successfully!")
-                st.text_area("Decoded Message:", value=decoded_message, height=150, disabled=True)
+                    st.success("Message decoded successfully!")
+                    st.text_area("Decoded Message:", value=decoded_message, height=150, disabled=True)
+            except:
+                st.warning("Please enter an image!")
+                
 
 if options == "Audio":
     st.title("Audio Steganography")
